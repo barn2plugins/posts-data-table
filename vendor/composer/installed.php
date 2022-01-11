6882c49b1fddc79602273409c705644fa0d49832
Andrew Keith <andy@barn2.co.uk>
2022-01-11 13:12:13 +0100

    Build plugin v1.3.7

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e9a650a3fcafa27e7faa3418db20955f949821f6',
+        'name' => 'barn2/posts-data-table',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'barn2/posts-data-table' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e9a650a3fcafa27e7faa3418db20955f949821f6',
+            'dev_requirement' => false,
+        ),
+    ),
+);
