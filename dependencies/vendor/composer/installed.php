5700aa1d8cbcc5df63fb952b60eedc0992dd1c52
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2022-12-09 16:59:50 +0000

    scope setup wizard

---
+++ dependencies/vendor/composer/installed.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Barn2\Plugin\Posts_Table_Search_Sort\Dependencies;
+
+return array('root' => array('name' => 'barn2/setup-wizard', 'pretty_version' => 'dev-master', 'version' => 'dev-master', 'reference' => 'a9303c63271cd3f3d655ccda25dfbf372375fce0', 'type' => 'library', 'install_path' => __DIR__ . '/../../', 'aliases' => array(), 'dev' => \false), 'versions' => array('barn2/setup-wizard' => array('pretty_version' => 'dev-master', 'version' => 'dev-master', 'reference' => 'a9303c63271cd3f3d655ccda25dfbf372375fce0', 'type' => 'library', 'install_path' => __DIR__ . '/../../', 'aliases' => array(), 'dev_requirement' => \false)));
