38c90da47303cb21fc7f44f5990ebe9fe8a0de0e
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2022-12-12 17:23:33 +0000

    scaffold fields

--- src/Admin/Wizard/Steps/Layout.php
+++ src/Admin/Wizard/Steps/Layout.php
@@ -4,6 +4,7 @@ namespace Barn2\Plugin\Posts_Table_Search_Sort\Admin\Wizard\Steps;
 
 use Barn2\Plugin\Posts_Table_Search_Sort\Dependencies\Barn2\Setup_Wizard\Api;
 use Barn2\Plugin\Posts_Table_Search_Sort\Dependencies\Barn2\Setup_Wizard\Step;
+use Barn2\PTS_Lib\Util;
 
 /**
  * Layout Settings Step.
@@ -14,7 +15,7 @@ use Barn2\Plugin\Posts_Table_Search_Sort\Dependencies\Barn2\Setup_Wizard\Step;
  * @copyright Barn2 Media Ltd
  */
 class Layout extends Step {
-
+    
 	/**
 	 * {@inheritdoc}
 	 */
@@ -29,7 +30,27 @@ class Layout extends Step {
 	 * {@inheritdoc}
 	 */
 	public function setup_fields() {
-		$fields = [];
+		$fields = [
+			'layout'  => [
+				'label'       => __( 'Post type', 'posts-data-table' ),
+				'description' => __( 'The default post type for your tables.', 'posts-data-table' ),
+				'type'        => 'select',
+				'options'     => [
+					[
+						'value' => 'post',
+						'label' => __( 'Post' ),
+					],
+				],
+				'value'       => 'post',
+				'premium'     => true,
+			],
+			'columns' => [
+				'label'       => __( 'Columns' ),
+				'description' => __( 'List the columns to include in your posts tables.' ) . ' ' . Util::barn2_link( 'kb/list-your-wordpress-blog-posts/#table-columns', esc_html__( 'Read more', 'document-library-lite' ), true ),
+				'type'        => 'text',
+				'value'       => '',
+			],
+		];
 
 		return $fields;
 	}
